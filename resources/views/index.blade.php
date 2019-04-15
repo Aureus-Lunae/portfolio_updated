d8504a47c9815b2175164260a3bdf4b59607c692
Erwin Korsten <mw.korsten@gmail.com>
2019-04-15 18:21:05 +0200

    Added a moon on the home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,29 @@
 
 @section('content')
 
-<div>
-	<p>
-		This is a test message. To see how everything looks.
-	</p>
+<div id="side_left" class="maxheight">
+
+
+	<div id="sidebar">
+		<div class="flex maxheight align_center">
+			<h1 class="maxwidth">Erwin Korsten</h1>
+			<h2 class="maxwidth">Back-end Web Developer</h2>
+		</div>
+	</div>
+
+
+	<div id="main" >
+		<div class="flex maxheight align_center justify_center">
+			<div class="moon-perspective">
+				<div class="moon-freya">
+
+					<span class="moon-overlay"></span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 </div>
 
 @endsection
